6be309aa1712fd68a5470c7596e6e6c156926506
Ibrahim <abu.selim404@gmail.com>
2023-09-07 14:28:19 -0700

    Hello, printf task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : This is C program that prints by Printf.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+}
