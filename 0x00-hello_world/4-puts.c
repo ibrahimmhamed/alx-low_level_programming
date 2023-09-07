89c30f83f8a432ba3e4505c9f2f11563402e4ab1
Ibrahim <abu.selim404@gmail.com>
2023-09-07 14:23:20 -0700

    update puts / commint to c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : This is C program that prints by Puts.
+ *
+ * Return: 0 (Success)
+*/
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
