398488a913a932b4e7fd519d53d13e255bc261f8
Ibrahim <abu.selim404@gmail.com>
2023-09-07 14:57:54 -0700

    update size.c task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,18 +3,17 @@
 /**
  * main - Entry point
  *
- * Description : This is C program that prints the size of various types on the computer it is compiled and run on.
+ * Description : This is C program that prints the size of various types.
  *
  * Return: 0 (Success)
 */
 
-
 int main(void)
 {
-	printf("Size of a", sizeof(char));
-	printf("Size of a", sizeof(int));
-	printf("Size of a", sizeof(long int));
-	printf("Size of a", sizeof(long long int));
-	printf("Size of a", sizeof(float));
+	printf("Size of a char: %u byte(s)\n", sizeof(char));
+	printf("Size of an int: %u byte(s)\n", sizeof(int));
+	printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
