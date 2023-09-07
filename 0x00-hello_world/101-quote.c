9659109162c146a8927704f0c3d65b3249b1c8fb
Ibrahim <abu.selim404@gmail.com>
2023-09-07 15:40:27 -0700

    last task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : This is C program that prints without using Printf & Puts.
+ *
+ * Return: 1 (Success)
+*/
+
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1, quo, 59);
+	return (1);
+}
