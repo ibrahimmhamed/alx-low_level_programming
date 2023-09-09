c44e1e4d9ecce48662cc48bea2715e0aefd34026
Ibrahim <abu.selim404@gmail.com>
2023-09-09 13:56:20 -0700

    add [9-print_comb.c] file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description : This program prints all possible combinations of digit num.
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+	char x = 0;
+
+	while (x < 9)
+	{
+		putchar (x + 48);
+		putchar (',');
+		putchar (' ');
+		x++;
+
+	if (x == 9)
+		putchar ('9');
+	}
+
+	return (0);
+}
