8cc9b2ead633669b700e55b49add40380ce9cc7e
Ibrahim <abu.selim404@gmail.com>
2023-09-09 14:15:32 -0700

    update v2 [9-print_comb.c] file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,16 +12,16 @@ int main(void)
 {
 	int x = 0;
 
-	while (x < 9)
+	while (x <= 9)
 	{
 		putchar (x + 48);
+
+	if (x != 9)
+	{
 		putchar (',');
 		putchar (' ');
-		x++;
-
-	if (x == 9)
-		putchar ('9');
 	}
-
+		x++;
+	}
 	return (0);
 }
