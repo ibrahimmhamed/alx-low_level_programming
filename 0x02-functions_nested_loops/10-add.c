0175834c7c6ca8c8625c81f1ddca0e38257e3845
Ibrahim <abu.selim404@gmail.com>
2023-09-12 16:51:03 -0700

    added all file without {9}

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ *
+ * @x: 1st number.
+ * @y: 2nd number.
+ *
+ * Return: result
+*/
+
+int add(int x, int y)
+{
+	int result = x + y;
+
+	return (result);
+}
