e0531b0dbf2252ab41bb0d75ccff1625c13cef69
Ibrahim <abu.selim404@gmail.com>
2023-09-12 14:23:02 -0700

    added [1-alphabet.c] file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ *
+ * Description : prints the alphabet, in lowercase by _putchar.
+*/
+
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+}
