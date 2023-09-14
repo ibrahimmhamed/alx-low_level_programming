54d7504c9136bb87d68bd85c3a795570545668bc
Ibrahim <abu.selim404@gmail.com>
2023-09-14 15:39:56 -0700

    t-isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit -  prints the last digit of a number.
+ *
+ * @c: takes input from other functions.
+ *
+ * Return: 1 if c  is a digit. and 0 otherwise.
+*/
+
+int _isdigit(int c);
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
