8655e9811073b3b0ee6f64d1245a0a5224ab5ad8
Ibrahim <abu.selim404@gmail.com>
2023-09-15 15:41:00 -0700

    uu

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,14 +11,14 @@ void print_diagonal(int n)
 	int x, y;
 
 	if (n < 1)
-		putchar('\n')
+		_putchar('\n')
 
 			for (x = 0; x < n; x++)
 			{
 				for (y = 0; y < n; y++)
 				{
-					putchar('\\');
+					_putchar('\\');
 				}
-				putchar('\n');
+				_putchar('\n');
 			}
 }
