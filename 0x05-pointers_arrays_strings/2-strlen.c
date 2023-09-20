fa6b79669b9c3e1bb200106d20af16e195be93d9
Ibrahim <abu.selim404@gmail.com>
2023-09-20 14:04:07 -0700

    task 2-str

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: an str as parameter.
+ *
+ * Return: 0 (Success)
+*/
+
+int _strlen(char *s)
+{
+	char s[16] = "My first strlen!";
+	int len;
+
+	len = strlen(s);
+	printf("%d\n", len);
+
+	return (0);
+}
