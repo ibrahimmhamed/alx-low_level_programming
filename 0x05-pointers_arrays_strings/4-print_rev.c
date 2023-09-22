f89f02a649f9adfb9973874f468e3132a736ae3b
Ibrahim <abu.selim404@gmail.com>
2023-09-22 00:59:37 -0700

    print rev task

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, followed by a newline.
+ *
+ * @s: an string parameter to print.
+*/
+
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		s++;
+	}
+	while (s > 0)
+	{
+		s--;
+		_putchar(*s + 0);
+	}
+	_putchar('\n');
+}
