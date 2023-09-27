9a754a75e92c099d6d05595e3c73f72f54144be1
Ibrahim <abu.selim404@gmail.com>
2023-09-27 14:33:28 -0700

    prototype _putchar

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,16 @@
 
 void print_number(int n)
 {
-	unsigned int x = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		x = -x;
+		n = -n;
 	}
 
-	if (x > 9)
+	if (n / 10 != 0)
 	{
-		print_number(x / 10);
+		print_number(n / 10);
 	}
 
-	_putchar((x % 10) + '0');
+	_putchar((n % 10) + 48);
 }
