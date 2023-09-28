293e1ba1e3b27059ad65f194025d2aa549ad5ff4
Ibrahim <abu.selim404@gmail.com>
2023-09-28 13:45:36 -0700

    update3

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,20 +4,17 @@
  * _puts_recursion - function that prints a string.
  *
  * @s: string.
- *
- * Return: stop recursion if s = NULL
 */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		return;
-	}
-	else
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+	{
+		_putchar('\n');
+	}
 }
