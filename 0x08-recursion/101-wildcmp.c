1470d7b309866fb19bd15ab0df4fa5921c804966
Ibrahim <abu.selim404@gmail.com>
2023-10-01 15:22:34 -0700

    the last update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -16,16 +16,21 @@ int wildcmp(char *s1, char *s2)
 		return (1);
 	}
 
-	if (*s1 == *s2 || *s2 == '*')
+	if (*s1 == *s2)
 	{
-		if (wildcmp(s1, s2 + 1))
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
 		{
-			return (1);
+			return (wildcmp(s1, s2 + 1));
 		}
 
-		if (*s1 != '\0')
+		if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
 		{
-			return (wildcmp(s1 + 1, s2 + 1));
+			return (1);
 		}
 	}
 
