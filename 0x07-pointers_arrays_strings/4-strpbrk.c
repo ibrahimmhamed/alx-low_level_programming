511d2912043622787396e087d751b912ff3f325f
Ibrahim <abu.selim404@gmail.com>
2023-10-04 09:04:48 -0700

    update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -13,19 +13,17 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int x, y;
+	unsigned int x, y;
 
-	x = 0;
-	while (s[x] != '\0')
+	for (x = 0; s[x] != '\0'; x++)
 	{
 		for (y = 0; accept[y] != '\0'; y++)
 		{
 			if (s[x] == accept[y])
 			{
-				return (s + x);
+				return (&s[x]);
 			}
 		}
-		x++;
 	}
 
 	return ('\0');
