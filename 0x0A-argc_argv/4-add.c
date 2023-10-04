8a7c027e704d3c8786554e538a645601ead39cc7
Ibrahim <abu.selim404@gmail.com>
2023-10-04 14:05:36 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - program that adds positive numbers.
+ *
+ * @argc: The number of command-line arguments
+ * @argv: array of argument strings
+ *
+ * Return: 1 if one of the number contains symbols that are not digits,
+ * otherwise 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, sum;
+
+	if (argc < 0)
+	{
+		printf("0\n");
+	}
+	else if (argc >= 1)
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		sum = num1 + num2;
+
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+
+	return (0);
+}
