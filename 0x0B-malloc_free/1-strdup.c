cd96504be66f66889439b9fc9068aea1a3d70f8d
Ibrahim <abu.selim404@gmail.com>
2023-10-04 15:56:30 -0700

    2-str_concat.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,40 @@
 #include "main.h"
 #include "stdlib.h"
 
-
 /**
- * *_strdup - 
+ * *_strdup - function that returns a pointer to
+ * a newly allocated space in memory, which contains
+ * a copy of the string given as a parameter.
  *
- * @str:
+ * @str: string
  *
- * Return: 
+ * Return: NULL if 'str' = NULL
 */
 
 char *_strdup(char *str)
 {
+	int x, y;
+	char *s;
+
+	if (str != NULL)
+	{
+		for (x = 0; str[x] != '\0'; x++)
+			;
+
+		s = malloc(x + 1 * sizeof(char));
+
+		if (s == NULL)
+		{
+			return (NULL);
+		}
+
+		for (y = 0; str[y] != '\0'; y++)
+		{
+			s[y] = str[y];
+		}
+
+		return (s);
+	}
+
+	return (NULL);
 }
