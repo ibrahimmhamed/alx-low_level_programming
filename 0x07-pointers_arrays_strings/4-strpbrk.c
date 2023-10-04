0b2ac8780116675b19fea4c8b459467d133561c2
Ibrahim <abu.selim404@gmail.com>
2023-10-04 09:10:22 -0700

    update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,11 +9,11 @@
  * Return: pointer to the byte in 's'
  * that matches one of the bytes in accept,
  * or 'NULL' if no such byte is found
- */
+*/
 
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int x, y;
+	int x, y;
 
 	x = 0;
 	while (s[x] != '\0')
