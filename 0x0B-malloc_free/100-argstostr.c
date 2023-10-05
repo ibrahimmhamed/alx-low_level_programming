a80d6872f229b96b4d8bc43e5aef8de8297b374d
Ibrahim <abu.selim404@gmail.com>
2023-10-04 18:09:54 -0700

    add file to advanced task

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * *argstostr - function that concatenates all the arguments of your program.
+ *
+ * @str:
+ *
+ * Return:
+*/
+
+char *argstostr(int ac, char **av);
+{
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "stdlib.h"
+
+/**
+ * **strtow - function that splits a string into words.
+ *
+ * @str:
+ *
+ * Return:
+*/
+
+char **strtow(char *str)
+{
+}
