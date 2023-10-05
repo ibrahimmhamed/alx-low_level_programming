19b9ca5ff9d91cf0d93d33b48d2fa9e3cabcdd35
Ibrahim <abu.selim404@gmail.com>
2023-10-04 18:51:04 -0700

    advanced task

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,11 +4,50 @@
 /**
  * *argstostr - function that concatenates all the arguments of your program.
  *
- * @str:
+ * @ac: integer
+ * @av: pointer to array
  *
- * Return:
+ * Return: NULL if ac == 0 or av == NULL,
+ * pointer to a new string, or NULL if it fails
 */
 
-char *argstostr(int ac, char **av);
+char *argstostr(int ac, char **av)
 {
+	int i, nc, x, y;
+	char *str;
+	x = y = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (nc = 0; av[i][nc]; nc++)
+			y++;
+	}
+
+	y += ac;
+
+	str = malloc(sizeof(char) * y + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (nc = 0; av[i][nc]; nc++)
+		{
+			str[x] = av[i][nc];
+			x++;
+		}
+		if (str[x] == '\0')
+		{
+			str[x++] = '\n';
+		}
+	}
+	return (str);
 }
