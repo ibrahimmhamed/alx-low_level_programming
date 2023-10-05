6dea43a852ab3ea86be3cc537c45c320cedee844
Ibrahim <abu.selim404@gmail.com>
2023-10-05 13:40:58 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,24 +15,24 @@ int main(int argc, char *argv[])
 {
 	int num1, num2, sum;
 
-	if (argc < 0)
+	if (argc < 3)
 	{
 		printf("0\n");
+		return (0);
 	}
-	else if (argc >= 1)
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		sum = num1 + num2;
 
-		printf("%d\n", sum);
-	}
-	else
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	if (num1 == 0 || num2 == 0)
 	{
 		printf("Error\n");
-
 		return (1);
 	}
+
+	sum = num1 + num2;
+
+	printf("%d\n", sum);
 
 	return (0);
 }
