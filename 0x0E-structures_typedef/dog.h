f0a5ed2f4cca2e6e68ae9d1d2732c39e8cd7a0ab
Ibrahim <abu.selim404@gmail.com>
2023-10-11 15:20:51 -0700

    start

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog structure
+ *
+ * @name: the name of dog
+ * @age: the age of dog
+ * @owner: the owner of dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
