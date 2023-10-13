8a03ac2df75ce2d1108cfc2d4772e874d055cfe1
Ibrahim <abu.selim404@gmail.com>
2023-10-12 18:28:14 -0700

    update important file

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,13 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - function that returns the sum of all its parameters
+ * sum_them_all - function that returns the sum of all its parameters.
  *
- * @separator: string
  * @n: num of integer
  *
  * Return: 0 if n equal 0, otherwise sum
 */
 
-void print_numbers(const char *separator, const unsigned int n, ...)
+int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
-	int x, sum = 0;
-
-	va_start(ap, n);
-
-	for (x = 0; x < n; x++)
-	{
-		sum +=  va_arg(ap, int);
-	}
-
-	va_end(ap);
-
-	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,1 +1,26 @@
-#
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that prints numbers, followed by a new line
+ * @separator: string
+ * @n: num of integer
+ *
+ * Return: 0 if n equal 0, otherwise sum
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int x, sum = 0;
+
+	va_start(ap, n);
+
+	for (x = 0; x < n; x++)
+	{
+		sum +=  va_arg(ap, int);
+	}
+
+	va_end(ap);
+
+	return (sum);
+}
